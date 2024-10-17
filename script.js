daab8b91fd374473045ed1dce7506da339812a8c
Luanaljm <luanaljm36@gmail.com>
2024-10-17 00:44:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-document.getElementById("form-sintomas").addEventListener("submit", function(event) {
+document.getElementById("form-primeiros-socorros").addEventListener("submit", function(event) {
     event.preventDefault();
 
     const nome = document.getElementById("nome").value;
-    const sintomas = document.getElementById("sintomas").value;
+    const primeirosSocorros = document.getElementById("primeiros-socorros").value;
     const intensidade = document.getElementById("intensidade").value;
 
     const resultado = document.getElementById("resultado");
-    resultado.innerHTML = `<p><strong>${nome}</strong>, seus sintomas foram registrados.</p>
-    <p><strong>Sintomas:</strong> ${sintomas}</p>
+    resultado.innerHTML = `<p><strong>${nome}</strong>, seus primeiros socorros foram registrados.</p>
+    <p><strong>Descrição:</strong> ${primeirosSocorros}</p>
     <p><strong>Intensidade:</strong> ${intensidade}</p>`;
 });
+
+function setActiveTab(element) {
+    const links = document.querySelectorAll("nav ul li a");
+    links.forEach(link => {
+        link.classList.remove("active");
+    });
+    element.classList.add("active");
+}
